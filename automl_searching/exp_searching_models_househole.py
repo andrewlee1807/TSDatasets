a7a01b1790f8eb281401e779cdefe99ff4f69d8f
Andrew <andrewlee1807@gmail.com>
2022-07-28 17:41:03 +0900

    test auto-tcn household in 20, 24, 36, 48, 60, 72, 94 tcn output

--- automl_searching/exp_searching_models_househole.py
+++ automl_searching/exp_searching_models_househole.py
@@ -1,10 +1,11 @@
 import sys
-sys.path.insert(0, '/home/andrew/Time Series/TSDatasets')
+
+sys.path.insert(0, '../')
 
 import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 import pandas as pd
 import numpy as np
@@ -17,14 +18,16 @@ import matplotlib.font_manager as fm
 
 from utils import HouseholdDataLoader, TSF_Data
 
-dataload = HouseholdDataLoader(data_path="/home/andrew/Time Series/dataset/Household_power_consumption/household_power_consumption.txt")
+dataload = HouseholdDataLoader(
+    data_path=r"../../dataset/Household_power_consumption/household_power_consumption.txt")
 data = dataload.data_by_hour
 
-result_path = "household_result_auto"
+list_dataset = ['household', 'spain', 'cnu']
+num_data = 2
+result_path = list_dataset[num_data] + '/household_result_auto'
 
 import keras_tuner as kt
 import pandas as pd
-
 
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.layers import Dense, LSTM
@@ -34,17 +37,20 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Input
 from tcn import TCN
 
+
 def model_builder(hp):
     kernel_size = hp.Choice('kernel_size', values=[2, 3, 5, 7])
-    nb_filters = hp.Choice('nb_filters', values=[16, 32, 64,128])
+    nb_filters = hp.Choice('nb_filters', values=[16, 32, 64, 128])
     use_skip_connections = hp.Choice(
         'use_skip_connections', values=[True, False])
 
     use_batch_norm = hp.Choice(
         'use_batch_norm', values=[True, False])
 
-    def temp(x): return 2**x
+    def temp(x): return 2 ** x
+
     def dilation_gen(x): return list(map(temp, range(x)))
+
     dilations = hp.Choice('dilations', values=list(range(2, 8)))
     # nb_stacks = hp.Choice('nb_stacks', values=[1, 2, 3, 4, 5])
     # nb_units_lstm = hp.Int('units_LSTM', min_value=32, max_value=320, step=32)
@@ -78,23 +84,25 @@ def model_builder(hp):
 
     return model_searching
 
+
 num_features = 1
 max_trials = 20
-input_width = 24
+input_width = 168
 
-for output_width in range(1, 25):
+for output_width in [20, 24, 36, 48, 60, 72, 94]:
     # Search model
-    exp_path = "HouseHold_TCN_Tune/Bayesian/"+str(output_width)+"/"
+    exp_path = "HouseHold_TCN_Tune/Bayesian/" + str(output_width) + "/"
     tuning_path = exp_path + "/models"
 
     if os.path.isdir(tuning_path):
         import shutil
+
         shutil.rmtree(tuning_path)
 
     tsf = TSF_Data(data=data['Global_active_power'],
-                input_width=input_width,
-                output_width=output_width,
-                train_ratio=0.9)
+                   input_width=input_width,
+                   output_width=output_width,
+                   train_ratio=0.9)
 
     tsf.normalize_data()
     input_width = tsf.data_train[0].shape[1]
@@ -112,20 +120,17 @@ for output_width in range(1, 25):
 
     # stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
 
-
     orig_stdout = sys.stdout
     f = open(result_path + f'/seaching_process_log_household_{str(output_width)}.txt', 'w')
     sys.stdout = f
 
     tuner.search(tsf.data_train[0], tsf.data_train[1],
-                validation_data=tsf.data_valid,
-                callbacks=[tf.keras.callbacks.TensorBoard(exp_path + "/log")],
-                epochs=10)
+                 validation_data=tsf.data_valid,
+                 callbacks=[tf.keras.callbacks.TensorBoard(exp_path + "/log")],
+                 epochs=10)
 
     # Get the optimal hyperparameters
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-
-    
 
     # Train real model_searching
     print(f"""
@@ -139,7 +144,6 @@ for output_width in range(1, 25):
     # Build the model with the optimal hyperparameters and train it on the data for 50 epochs
     model_best = tuner.hypermodel.build(best_hps)
 
-
     print('Train...')
 
     callbacks = [
@@ -148,28 +152,28 @@ for output_width in range(1, 25):
     ]
 
     history = model_best.fit(x=tsf.data_train[0],
-                            y=tsf.data_train[1],
-                            validation_data=tsf.data_valid,
-                            epochs=100,
-                            callbacks=[callbacks],
-                            verbose=2,
-                            use_multiprocessing=True)
+                             y=tsf.data_train[1],
+                             validation_data=tsf.data_valid,
+                             epochs=100,
+                             callbacks=[callbacks],
+                             verbose=2,
+                             use_multiprocessing=True)
 
     print("=============================================================")
     print("Minimum val mse:")
     print(min(history.history['val_mse']))
     print("Minimum val mae:")
     print(min(history.history['val_mae']))
-    model_best.evaluate(tsf.data_test[0],tsf.data_test[1], batch_size=1,
-               verbose=2,
-               use_multiprocessing=True)
+    model_best.evaluate(tsf.data_test[0], tsf.data_test[1], batch_size=1,
+                        verbose=2,
+                        use_multiprocessing=True)
     sys.stdout = orig_stdout
     f.close()
 
-    pd.DataFrame.from_dict(history.history).to_csv(result_path + '/history'+str(output_width) +'.csv',index=False)
-
+    pd.DataFrame.from_dict(history.history).to_csv(result_path + '/history' + str(output_width) + '.csv', index=False)
 
     from matplotlib import pyplot as plt
+
     plt.plot(history.history['mse'][5:])
     plt.plot(history.history['val_mse'][5:])
 
